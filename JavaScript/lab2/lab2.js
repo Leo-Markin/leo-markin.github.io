422bcc43ec69dccfd86f942413281cdafc87d5b8
Leo-Markin <120329470+Leo-Markin@users.noreply.github.com>
2023-05-01 14:17:52 +0300

    lab2

---
+++ JavaScript/lab2/lab2.js
@@ -0,0 +1,44 @@
+function pow(x,n) { // x - число возводимое в степень; n - степень
+    let res = 1;
+    if (n >= 0) for (let i = 0; i < n; i++) res *= x; // возведение в неотрицательную степень
+    else for (let i = 0; i > n; i--) res /= x;  // возведение в отрицательную степень
+    return res;
+}
+
+function sumTo(n) { // n - число до которого нужно просуммировать ряд n;
+    let sum = 0;
+    for (let i = 1; i <= n; i++) sum += i; // суммирование
+    return sum;
+}
+
+function factorial(n) { // n - число, факториал которого надо найти
+    if (BigInt(n) === 0n) return 1n; // 0! = 1
+    else return BigInt(n) * factorial(n-1);
+}
+
+function fib(n) { // n - номер числа фибоначчи
+    let a = 0n, b = 1n, c;
+    if (n == 0) return a; // 0ое число - 0
+    if (n == 1) return b; // 1ое число - 1
+    for (let i = 1; i < n; i++) { // вычисляем n-ое число фибоначчи
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return c;
+}
+
+function compare(x) { // x - сравниваемое число
+    return function(y) { // возвращаем анонимную функцию со сравнением. y - второе сравниваемое число
+        if (y > x) return true;
+        if (y < x) return false;
+        return null;
+    }
+}
+
+function sum(...args) { // args - n-ое кол-во аргументов
+    let s = args.reduce(function (currentSum, currentNum) { // складываем все элементы массива в currentSum. Аналог алгоритма accumulate() из C++
+        return currentSum + currentNum;
+    });
+    return s;
+}
